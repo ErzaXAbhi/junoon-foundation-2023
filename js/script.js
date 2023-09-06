cbec69407d91937bcd1ee12d855500791679b5d4
Abhijit Parad <ErzaXAbhi>
2023-09-06 23:13:49 +0530

    sections updated

--- js/script.js
+++ js/script.js
@@ -26,3 +26,23 @@ function revealAnime() {
     .from(heroPara, { autoAlpha: 0, y: -50, delay: 0 })
     .from(homeBtn, { autoAlpha: 0, y: -50, delay: 0 });
 }
+
+// reveal on scroll animation js code
+
+window.addEventListener("scroll", reveal);
+
+function reveal() {
+  let reveals = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let revealTop = reveals[i].getBoundingClientRect().top;
+    let revealPoint = 150;
+
+    if (revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
